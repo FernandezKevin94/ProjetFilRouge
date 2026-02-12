9e58890b1fcf4d51cdac36f7cbfeeb05edc7c665
Kevin Fernandez <kevinfernandeznueve9@gmail.com>
2026-02-12 11:07:37 +0100

    removed env from files now hidden

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -56,7 +56,7 @@ final class UserController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_login', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/newUser.html.twig', [
